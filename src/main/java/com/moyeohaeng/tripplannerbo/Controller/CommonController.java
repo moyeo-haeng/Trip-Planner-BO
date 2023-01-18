842044028bbe53f908e00b087670bcece2ae2920
kjkj2089 <62068368+kjkj2089@users.noreply.github.com>
2023-01-19 00:54:24 +0900

    포트번호 변경, 메인페이지 연결, 부트스트랩 파일 추가

---
+++ src/main/java/com/moyeohaeng/tripplannerbo/Controller/CommonController.java
@@ -0,0 +1,13 @@
+package com.moyeohaeng.tripplannerbo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CommonController {
+
+    @GetMapping("/")
+    public String home() {
+        return "login";
+    }
+}
---
+++ src/main/java/com/moyeohaeng/tripplannerbo/Controller/MemberController.java
@@ -0,0 +1,7 @@
+package com.moyeohaeng.tripplannerbo.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+}
