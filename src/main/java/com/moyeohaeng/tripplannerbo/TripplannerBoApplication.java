2c6fab6a25b6a4d74beda413cbda4dac3aba34a6
김민지 <9mjkim@gmail.com>
2023-01-18 23:46:08 +0900

    first commit

---
+++ src/main/java/com/moyeohaeng/tripplannerbo/TripplannerBoApplication.java
@@ -0,0 +1,13 @@
+package com.moyeohaeng.tripplannerbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripplannerBoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripplannerBoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moyeohaeng/tripplannerbo/TripplannerBoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyeohaeng.tripplannerbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripplannerBoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
