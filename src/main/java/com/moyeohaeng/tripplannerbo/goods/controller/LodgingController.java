1c66fc229d5d29ee314a75f82d635f791f64555a
이동우 <dongwoo7332@gmail.com>
2023-01-29 21:04:05 +0900

    [lod-1] 숙소관리/등록 틀 만들어보기~

--- src/main/java/com/moyeohaeng/tripplannerbo/goods/controller/LodgingController.java
+++ src/main/java/com/moyeohaeng/tripplannerbo/goods/controller/LodgingController.java
@@ -1,4 +1,23 @@
 package com.moyeohaeng.tripplannerbo.goods.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/goods")
+@Controller
 public class LodgingController {
+
+    @GetMapping("/lodgingMgmt")
+    public String lodgingMgmt(){
+        return "goods/lodging_mgmt";
+    }
+
+    // 등록/상세페이지
+    @GetMapping("/lodgingRegist")
+    public String lodgingRegist(){
+        return "goods/lodging_regist";
+    }
+
 }
+
