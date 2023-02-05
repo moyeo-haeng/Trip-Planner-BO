9d67ad5bf708c16a9f7a8690dc88c6de7f1dcb1c
이동우 <dongwoo7332@gmail.com>
2023-02-05 21:32:28 +0900

    [dev] 이동우 / 샘플파일 이름 수정

--- src/main/java/com/moyeohaeng/tripplannerbo/sample/controller/SampleController.java
+++ src/main/java/com/moyeohaeng/tripplannerbo/sample/controller/SampleController.java
@@ -1,14 +1,10 @@
 package com.moyeohaeng.tripplannerbo.sample.controller;
 
-import com.moyeohaeng.tripplannerbo.common.util.CommonUtil;
 import com.moyeohaeng.tripplannerbo.common.utility.CommonUtility;
-import com.moyeohaeng.tripplannerbo.sample.domain.SampleModel;
 import com.moyeohaeng.tripplannerbo.sample.service.SampleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,7 +18,7 @@ public class SampleController extends CommonUtility {
 
 //        List<SampleModel> sampleModelList = sampleService.sample();
 
-        return "/sample/sample";
+        return "sample/CommonCodeBoxSample";
     }
 
 }
