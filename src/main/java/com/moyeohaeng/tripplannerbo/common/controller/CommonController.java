c909ccee45282f700752abe4c1ee6be1ce23d97c
최지연 <cjyeon@pionnet.co.kr>
2023-02-12 23:35:13 +0900

    [comm-8] Spring MessageResource 사용 코드 추가

--- src/main/java/com/moyeohaeng/tripplannerbo/common/controller/CommonController.java
+++ src/main/java/com/moyeohaeng/tripplannerbo/common/controller/CommonController.java
@@ -2,15 +2,22 @@ package com.moyeohaeng.tripplannerbo.common.controller;
 
 import com.moyeohaeng.tripplannerbo.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Locale;
 
 @Controller
 @RequiredArgsConstructor
 public class CommonController {
 
     private final MenuService menuService;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/")
     public String login() {
@@ -41,6 +48,8 @@ public class CommonController {
     @GetMapping("/message")
     public String messagePageRender(Model model) {
         model.addAttribute("price" , 1000);
+        model.addAttribute("test", messageSource.getMessage("error", new Object[]{}, Locale.getDefault()));
+        model.addAttribute("hello", messageSource.getMessage("replace.hello", new String[]{"name"}, Locale.getDefault()));
         return "sample/message";
     }
 }
